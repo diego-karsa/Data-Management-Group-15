e50e156ee9677cec3c9378cd623d90421da00158
Amornratt <amornrat.taraporntip@warwick.ac.uk>
2024-03-13 13:09:12 +0000

    Updates

--- R/db_creation.R
+++ R/db_creation.R
@@ -1,153 +1,118 @@
-
-library(RSQLite)
 library(readr)
+library(RSQLite)
+library(dplyr)
+library(DBI)
+library(DBI)
+library(RSQLite)
 
-# Connect to the database (this will create the database if it doesn't exist)
+ecom_db <- dbConnect(RSQLite::SQLite(), "/cloud/project/DATABASE/ecom.db")
 
-db_path = "DATABASE/ecom.db"
+tables_to_drop <- c("advertisement", "order_details", "orders", "delivery", "transactions", "product", "customer", "supplier", "category", "address", "payment", "product_supplier")
+for (table in tables_to_drop) {
+  dbExecute(ecom_db, paste0("DROP TABLE IF EXISTS ", table, ";"))
+}
 
-db <- dbConnect(SQLite(), dbname = db_path)
-
-# SQL commands to create the schema
-
-sql_commands <- c(
-  "DROP TABLE IF EXISTS advertisement;",
-  "DROP TABLE IF EXISTS product_supplier;",
-  "DROP TABLE IF EXISTS supplier;",
-  "DROP TABLE IF EXISTS payment;",
-  "DROP TABLE IF EXISTS order_details;",
-  "DROP TABLE IF EXISTS customer;",
-  "DROP TABLE IF EXISTS product;",
-  "DROP TABLE IF EXISTS address;",
-  "DROP TABLE IF EXISTS category;",
-  
+create_statements <- c(
+  "CREATE TABLE customer (
+    customer_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    customer_phone VARCHAR(30) NOT NULL,
+    customer_email VARCHAR(50) NOT NULL,
+    cutomer_unit VARCHAR(10),
+    cutomer_floor INT,
+    cutomer_block VARCHAR(20),
+    cutomer_street VARCHAR(100) NOT NULL,
+    cutomer_city VARCHAR(60),
+    cutomer_country VARCHAR(40) NOT NULL,
+    cutomer_zip_code VARCHAR(20) NOT NULL
+  );",
+  "CREATE TABLE supplier (
+    supplier_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    supplier_name VARCHAR(255) NOT NULL,
+    supplier_phone VARCHAR(30) NOT NULL,
+    supplier_email VARCHAR(50) NOT NULL,
+    supplier_unit VARCHAR(10),
+    supplier_floor INT,
+    supplier_block VARCHAR(20),
+    supplier_street VARCHAR(100) NOT NULL,
+    supplier_city VARCHAR(60),
+    supplier_country VARCHAR(40) NOT NULL,
+    supplier_zip_code VARCHAR(20) NOT NULL
+  );",
   "CREATE TABLE category (
-    category_id INT PRIMARY KEY,
-    category_name VARCHAR(255) NOT NULL,
-    parent_category_id INT,
+    category_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    category_name VARCHAR(20) NOT NULL,
     category_description TEXT,
+    parent_category_id VARCHAR(100),
     FOREIGN KEY (parent_category_id) REFERENCES category(category_id)
   );",
-  
-  "CREATE TABLE address (
-    address_id INT PRIMARY KEY,
-    house_number INT NOT NULL,
-    street VARCHAR(255) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    Post_Code VARCHAR(20) NOT NULL,
-    country VARCHAR(100) NOT NULL
-  );",
-  
   "CREATE TABLE product (
-    product_id INT PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,
-    product_description TEXT,
+    product_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    supplier_id VARCHAR(100) NOT NULL,
+    category_id VARCHAR(100) NOT NULL,
+    product_name VARCHAR(60) NOT NULL,
+    product_description VARCHAR(255),
     product_price DECIMAL(10, 2) NOT NULL,
-    stockquantity INT NOT NULL,
-    category_id INT,
+    product_stock INT NOT NULL,
+    rating DECIMAL(3, 2),
+    is_discounted BOOLEAN,
+    FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id),
     FOREIGN KEY (category_id) REFERENCES category(category_id)
   );",
-  
-  "CREATE TABLE customer (
-    customer_id INT PRIMARY KEY,
-    customer_email VARCHAR(255) NOT NULL,
-    customer_phone VARCHAR(13) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    address_id INT,
-    UNIQUE (customer_email),
-    UNIQUE (customer_phone),
-    FOREIGN KEY (address_id) REFERENCES address(address_id)
-  );",
-  
-  "CREATE TABLE order_details (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    product_id INT,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (product_id) REFERENCES product(product_id)
-  );",
-  
-  "CREATE TABLE payment (
-    payment_id INT PRIMARY KEY,
-    customer_id INT,
-    product_id INT,
-    order_id INT,
-    amount DECIMAL(10, 2) NOT NULL,
-    payment_date DATE NOT NULL,
-    payment_type VARCHAR(20) NOT NULL CHECK(payment_type IN('card', 'cash')),
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (order_id) REFERENCES order_details(order_id),
-    FOREIGN KEY (product_id) REFERENCES product(product_id)
-  );",
-  
-  "CREATE TABLE supplier (
-    supplier_id INT PRIMARY KEY,
-    supplier_name VARCHAR(255) NOT NULL,
-    supplier_email VARCHAR(255),
-    supplier_phone VARCHAR(20),
-    address_id INT NOT NULL,
-    UNIQUE (supplier_phone),
-    FOREIGN KEY (address_id) REFERENCES address(address_id)
-  );",
-  
-  "CREATE TABLE product_supplier (
-    product_supplier_id INT PRIMARY KEY,
-    product_id INT,
-    supplier_id INT,
-    supplier_price DECIMAL(10, 2) NOT NULL,
-    supply_date DATE NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product(product_id),
-    FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id)
-  );",
-  
   "CREATE TABLE advertisement (
-    ad_id VARCHAR(255) PRIMARY KEY,
-    product_id INT,
-    ad_url VARCHAR(255) NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product(product_id)
+    ad_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    product_id VARCHAR(100) NOT NULL,
+    url VARCHAR(255),
+    ad_start_date DATE,
+    ad_end_date DATE,
+    discount DECIMAL(5, 4),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    UNIQUE (product_id)
+  );",
+  "CREATE TABLE orders (
+    order_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    customer_id VARCHAR(100) NOT NULL,
+    order_date DATE NOT NULL,
+    status_of_order TEXT CHECK (status_of_order IN ('Processed', 'Cancelled')) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+  );",
+  "CREATE TABLE order_details (
+    order_id VARCHAR(100) NOT NULL,
+    product_id VARCHAR(100) NOT NULL,
+    quantity_of_product INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    PRIMARY KEY (order_id, product_id)
+  );",
+  "CREATE TABLE delivery (
+    delivery_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    order_id VARCHAR(100) NOT NULL,
+    delivery_type TEXT CHECK (delivery_type IN ('Standard', 'Premium')) NOT NULL,
+    dispatched_date DATE,
+    delivered_date DATE,
+    delivery_status TEXT CHECK (delivery_status IN ('Pending', 'Shipped', 'Delivered')) NOT NULL,
+    delivery_cost DECIMAL(6,2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+  );",
+  "CREATE TABLE transactions (
+    transactions_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    order_id VARCHAR(100) NOT NULL,
+    transaction_date DATE NOT NULL,
+    payment_type TEXT CHECK (payment_type IN ('Debit/Credit Card')) NOT NULL,
+    card_number VARCHAR(16) NOT NULL,
+    expiry_date DATE,
+    cvv CHAR(3),
+    payment_status TEXT CHECK (payment_status IN ('Pending', 'Processing', 'Processed')) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
   );"
-  
 )
 
-# Execute each SQL command
-
-for (sql in sql_commands) {
-  
-  dbExecute(db, sql)
-  
+for (statement in create_statements) {
+  dbExecute(ecom_db, statement)
 }
 
-# List of CSV files and their corresponding table names
-files_and_tables <- list(
-  "address.csv" = "address",
-  "advertisement.csv" = "advertisement",
-  "category.csv" = "category",
-  "customer.csv" = "customer",
-  "order_details.csv" = "order_details",
-  "payment.csv" = "payment",
-  "product_supplier.csv" = "product_supplier",
-  "product.csv" = "product",
-  "supplier.csv" = "supplier"
-)
 
-# Base path where CSV files are stored
-base_path <- "MOCKDATA/"
 
-# Loop through each file and table name pair, import the CSV file into R, and then write to the SQLite table
-for (file in names(files_and_tables)) {
-  # Full path to the CSV file
-  file_path <- paste0(base_path, file)
-  # Read the CSV file into a data frame
-  data_frame <- read_csv(file_path)
-  # Write the data frame to the SQLite table
-  # This assumes table names and CSV file column names match the database schema
-  dbWriteTable(db, files_and_tables[[file]], data_frame, append = TRUE, row.names = FALSE)
-}
-
-# Disconnect from the database
-dbDisconnect(db)
-
+dbDisconnect(ecom_db)
